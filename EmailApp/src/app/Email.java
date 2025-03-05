6209f3213bb60d19fbe7b22418e8378af0f7afd2
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2025-03-05 10:59:32 +0100

    properties and constructor added to class

--- EmailApp/src/app/Email.java
+++ EmailApp/src/app/Email.java
@@ -1,5 +1,15 @@
 package app;
 
 public class Email {
-
+	private String name;
+	private String surname;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	public Email(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
 }
