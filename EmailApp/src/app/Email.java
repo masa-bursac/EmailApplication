812870901b833ca1709bea3a41b5832c9c1e6f42
masa.bursac <bursac.ra133.2018@uns.ac.rs>
2025-03-05 11:23:53 +0100

    generate department implemented

--- EmailApp/src/app/Email.java
+++ EmailApp/src/app/Email.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.*;
+
 public class Email {
 	private String name;
 	private String surname;
@@ -11,5 +13,21 @@ public class Email {
 	public Email(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
+		System.out.println("EMAIL CREATED: " + this.name + " " + this.surname);
+		
+		this.department = generateDepartment();
+		System.out.println("Department: " + this.department);
+	}
+	
+	private String generateDepartment() {
+		System.out.println("Enter the department code:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None");
+		Scanner in = new Scanner(System.in);
+		
+		int input = in.nextInt();
+		
+		if(input == 1) { return "sales"; }
+		else if(input == 2) { return "development"; }
+		else if(input == 3) { return "accounting"; }
+		else { return ""; }
 	}
 }
